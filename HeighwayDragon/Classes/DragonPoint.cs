4beb2ac23ff08e6257693abd054a3dd28d90f155
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-02-13 23:08:05 +0100

    Refactor of the Dragon and DragonPoint classes

--- HeighwayDragon/Classes/DragonPoint.cs
+++ HeighwayDragon/Classes/DragonPoint.cs
@@ -1,45 +1,41 @@
-﻿// Copyright (c) Lukasz Pyrzyk
-// https://linkedin.com/in/lukaszpyrzyk
-// All Rights Reserved
-// Licensed under the MIT License
-
-using System;
+﻿using System;
 
 namespace HeighwayDragon.Classes
 {
-	public class DragonPoint
-	{
-		public DragonPoint(double x, double y)
-		{
-			X = x;
-			Y = y;
-		}
+    public class DragonPoint
+    {
+        public double X { get; }
+        public double Y { get; }
 
-		public double X { get; private set; }
-		public double Y { get; private set; }
+        public DragonPoint(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
 
-		public int CalculateXPosition(double minValueInPoints, double maxValueInPoints, int width)
-		{
-			if (X != maxValueInPoints)
-			{
-				double distance = maxValueInPoints - minValueInPoints;
-				double proportionToMaxWidth = (maxValueInPoints - X) / distance;
-				double position = proportionToMaxWidth * width;
-				return (int)Math.Ceiling(position) - 1;
-			}
-			return width - 1;
-		}
+        // TODO extract transformation to the UI
+        public int CalculateXPosition(double minValueInPoints, double maxValueInPoints, int width)
+        {
+            if (X != maxValueInPoints)
+            {
+                double distance = maxValueInPoints - minValueInPoints;
+                double proportionToMaxWidth = (maxValueInPoints - X) / distance;
+                double position = proportionToMaxWidth * width;
+                return (int)Math.Ceiling(position) - 1;
+            }
+            return width - 1;
+        }
 
-		public int CalculateYPosition(double minValueInPoints, double maxValueInPoints, int width)
-		{
-			if (Y != maxValueInPoints)
-			{
-				double distance = maxValueInPoints - minValueInPoints;
-				double proportionToMaxWidth = (maxValueInPoints - Y) / distance;
-				double position = proportionToMaxWidth * width;
-				return (int)Math.Ceiling(position) - 1;
-			}
-			return width - 1;
-		}
-	}
+        public int CalculateYPosition(double minValueInPoints, double maxValueInPoints, int width)
+        {
+            if (Y != maxValueInPoints)
+            {
+                double distance = maxValueInPoints - minValueInPoints;
+                double proportionToMaxWidth = (maxValueInPoints - Y) / distance;
+                double position = proportionToMaxWidth * width;
+                return (int)Math.Ceiling(position) - 1;
+            }
+            return width - 1;
+        }
+    }
 }
