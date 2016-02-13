278a229de2496abe06ff3fc64ec9c3b0d29812f2
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-02-13 23:50:09 +0100

    Unit tests for models

--- Tests/HeighwayDragon.Tests/Calculations/CSharpCalculatorTests.cs
+++ Tests/HeighwayDragon.Tests/Calculations/CSharpCalculatorTests.cs
@@ -13,7 +13,7 @@ namespace HeighwayDragon.Tests.Calculations
         [Test]
         public void CalculateX1_ReturnsCorrectValue()
         {
-            DragonCalculator calculator = new CSharpCalculator();
+            DragonCalculator calculator = new DragonCalculator();
 
             float x = _fixture.Create<float>();
             float y = _fixture.Create<float>();
@@ -28,7 +28,7 @@ namespace HeighwayDragon.Tests.Calculations
         [Test]
         public void CalculateY1_ReturnsCorrectValue()
         {
-            DragonCalculator calculator = new CSharpCalculator();
+            DragonCalculator calculator = new DragonCalculator();
 
             float x = _fixture.Create<float>();
             float y = _fixture.Create<float>();
@@ -43,7 +43,7 @@ namespace HeighwayDragon.Tests.Calculations
         [Test]
         public void CalculateX2_ReturnsCorrectValue()
         {
-            DragonCalculator calculator = new CSharpCalculator();
+            DragonCalculator calculator = new DragonCalculator();
 
             float x = _fixture.Create<float>();
             float y = _fixture.Create<float>();
@@ -59,7 +59,7 @@ namespace HeighwayDragon.Tests.Calculations
         [Test]
         public void CalculateY2_ReturnsCorrectValue()
         {
-            DragonCalculator calculator = new CSharpCalculator();
+            DragonCalculator calculator = new DragonCalculator();
 
             float x = _fixture.Create<float>();
             float y = _fixture.Create<float>();
---
+++ Tests/HeighwayDragon.Tests/Models/DragonPointTests.cs
@@ -0,0 +1,73 @@
+﻿using HeighwayDragon.Models;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace HeighwayDragon.Tests.Models
+{
+    [TestFixture]
+    public class DragonPointTests
+    {
+        private readonly Fixture _fixture = new Fixture();
+
+        [Test]
+        public void CanAssingValuesByCtor()
+        {
+            double x = _fixture.Create<double>();
+            double y = _fixture.Create<double>();
+
+            DragonPoint point = new DragonPoint(x, y);
+            Assert.That(point.X, Is.EqualTo(x));
+            Assert.That(point.Y, Is.EqualTo(y));
+        }
+
+
+        [Test]
+        public void CalculatesCorrectXPosition()
+        {
+            int min = 0;
+            int max = 99;
+            int width = 100;
+            DragonPoint point = new DragonPoint(50, 50);
+            int newPosition = point.CalculateXPosition(min, max, width);
+
+            Assert.That(newPosition, Is.EqualTo(49));
+        }
+
+        [Test]
+        public void CalculatesCorrectXPositionWhenXIsTheHighestValue()
+        {
+            int min = 0;
+            int max = 99;
+            int width = 100;
+            DragonPoint point = new DragonPoint(99, 99);
+            int newPosition = point.CalculateXPosition(min, max, width);
+
+            Assert.That(newPosition, Is.EqualTo(99));
+        }
+
+        [Test]
+        public void CalculatesCorrectYPosition()
+        {
+            int min = 0;
+            int max = 99;
+            int height = 100;
+            DragonPoint point = new DragonPoint(50, 50);
+            int newPosition = point.CalculateYPosition(min, max, height);
+
+            Assert.That(newPosition, Is.EqualTo(49));
+        }
+
+        [Test]
+        public void CalculatesCorrectXPositionWhenYIsTheHighestValue()
+        {
+            int min = 0;
+            int max = 99;
+            int width = 100;
+            DragonPoint point = new DragonPoint(99, 99);
+            int newPosition = point.CalculateYPosition(min, max, width);
+
+            Assert.That(newPosition, Is.EqualTo(99));
+        }
+
+    }
+}
